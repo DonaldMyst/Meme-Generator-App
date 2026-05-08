6d0df72288e005f92cd344dbdfc8ff2ce8abdc4d
DonaldMyst <donaldugboi@gmail.com>
2026-05-08 04:30:53 -0700

    Add files via upload

---
+++ Meme Generator App/script.js
@@ -0,0 +1,25 @@
+const generateMemeBtn = document.querySelector(".generate-meme-btn");
+const memeTitle = document.querySelector(".meme-title");
+const memeImage = document.querySelector(".meme-generator img");
+const memeAuthor = document.querySelector(".meme-author");
+
+const updateDetails = (url, title, author) => {
+    memeImage.setAttribute("src", url);
+    memeTitle.innerHTML = title;
+    memeAuthor.innerHTML = `Meme by: ${author}`;
+}
+
+const generateMeme = () => {
+    fetch("https://meme-api.com/gimme/wholesomememes")
+        .then((response) => response.json())
+        .then((data) => {
+                updateDetails(data.url, data.title, data.author);
+            })
+        .catch((error) => {
+            console.error("Error fetching meme:", error);
+        });
+};
+
+generateMemeBtn.addEventListener("click", generateMeme);
+
+generateMeme();
